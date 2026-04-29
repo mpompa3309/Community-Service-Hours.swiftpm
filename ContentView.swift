02acc3bf44b6b077d3eae2ccc2053f387f157dc2
Vighnesh Manchikanti <viggytheking@gmail.com>
2026-04-29 14:25:24 -0500

    Vighnesh- Began adding starting UI variables like Goal hours, completed hours, and progress value I also started the text field to enter goal hours

--- ContentView.swift
+++ ContentView.swift
@@ -1,8 +1,12 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var goalHours = ""
+    @State private var completedHours = ""
+    @State private var progressValue: Double = 0.0
     var body: some View {
         Text("Community Service Tracker")
+        TextField("Goal hours", text: $goalHours)
     }
 }
 #Preview {

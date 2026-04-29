386a23c85719a9ae5a20c49d9db71dad0737f113
Marcelo Pompa <marcelofpompa@gmail.com>
2026-04-29 14:51:44 -0500

    marcelo: added appstorage for persistance and .keyboardtype as it will be a mobile app

--- ContentView.swift
+++ ContentView.swift
@@ -1,17 +1,23 @@
 import SwiftUI
+import Foundation
 
 struct ContentView: View {
-    @State private var goalHours = ""
-    @State private var completedHours = ""
+    @AppStorage("goalHours") private var goalHours = ""
+    @AppStorage("completedHours") private var completedHours = ""
     @State private var progressValue: Double = 0.0
+    @State private var progressMessage = "0%"
+    
     var body: some View {
         VStack{
             Text("Community Service Tracker")
             TextField("Goal hours", text: $goalHours)
                 .textFieldStyle(.roundedBorder)
+                .keyboardType(.decimalPad)
             
             TextField("Completed hours", text: $completedHours)
                 .textFieldStyle(.roundedBorder)
+                .keyboardType(.decimalPad)
+            
         }
         .padding()
 }

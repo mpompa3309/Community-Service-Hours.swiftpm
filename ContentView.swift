6b9f7e37df993c772f99ef90a940a9e6198472db
Vighnesh Manchikanti <viggytheking@gmail.com>
2026-04-29 14:32:18 -0500

    Vighnesh- finshed textfields and added padding and vstack

--- ContentView.swift
+++ ContentView.swift
@@ -5,10 +5,18 @@ struct ContentView: View {
     @State private var completedHours = ""
     @State private var progressValue: Double = 0.0
     var body: some View {
-        Text("Community Service Tracker")
-        TextField("Goal hours", text: $goalHours)
-    }
+        VStack{
+            Text("Community Service Tracker")
+            TextField("Goal hours", text: $goalHours)
+                .textFieldStyle(.roundedBorder)
+            
+            TextField("Completed hours", text: $completedHours)
+                .textFieldStyle(.roundedBorder)
+        }
+        .padding()
 }
+    }
+     
 #Preview {
     ContentView()
 }
